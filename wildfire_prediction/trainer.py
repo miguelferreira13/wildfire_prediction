475253c1f7118966c9060ad38a13052507c9f696
Miguel Ferreira <82802408+miguelferreira13@users.noreply.github.com>
2021-06-09 09:39:57 +0200

    Baseline model

--- wildfire_prediction/trainer.py
+++ wildfire_prediction/trainer.py
@@ -1,7 +1,12 @@
 from google.cloud import storage
 import pandas as pd
-from sklearn import linear_model
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 import joblib
 
 ### GCP configuration - - - - - - - - - - - - - - - - - - -
@@ -34,19 +39,51 @@ def get_data():
     return df
 
 
-def preprocess(df):
+def train_model(df):
     """method that pre-process the data"""
-    X_train = df[["distance"]]
-    y_train = df["fare_amount"]
-    return X_train, y_train
+    df.Estimated_fire_area.fillna(0, inplace=True)
+    df.Mean_estimated_fire_brightness.fillna(0, inplace=True)
+    df.Mean_estimated_fire_radiative_power.fillna(0, inplace=True)
+    
+    X = df.drop(columns=['Estimated_fire_area', 'Date_x'])
+    y = df.Estimated_fire_area
+    
+    imputer = KNNImputer()
+    
+    numeric_features = X.dtypes[(X.dtypes == 'float64') | (X.dtypes == 'int64')].index
+    categorical_features = X.dtypes[(X.dtypes != 'float64') | (X.dtypes != 'int64')].index
 
 
-def train_model(X_train, y_train):
-    """method that trains the model"""
-    rgs = linear_model.Lasso(alpha=0.1)
-    rgs.fit(X_train, y_train)
-    print("trained model")
-    return rgs
+    numeric_transformer = Pipeline(steps=[
+    ('imputer', KNNImputer()),
+    ('scaler', StandardScaler())])
+
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+
+    preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ],
+        remainder='passthrough'
+        )
+
+    model = KNeighborsRegressor()
+
+    pipe = Pipeline([
+        ('pre', preprocessor),
+        ('model', model),
+    ])
+    
+    return pipe.fit(X, y)
+
+
+# def train_model(X_train, y_train):
+#     """method that trains the model"""
+    
+#     model.fit(X_train, y_train)
+#     print("trained model")
+#     return model
 
 
 STORAGE_LOCATION = 'models/wildfire_prediction/model.joblib'
@@ -83,12 +120,12 @@ if __name__ == '__main__':
     df = get_data()
 
     # preprocess data
-    X_train, y_train = preprocess(df)
+    # X_train, y_train = preprocess(df)
 
     # train model (locally if this file was called through the run_locally command
     # or on GCP if it was called through the gcp_submit_training, in which case
     # this package is uploaded to GCP before being executed)
-    reg = train_model(X_train, y_train)
+    model = train_model(df)
 
     # save trained model to GCP bucket (whether the training occured locally or on GCP)
-    save_model(reg)
+    save_model(model)

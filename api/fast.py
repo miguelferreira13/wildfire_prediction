2e7af6d2710006f2479bb14bfb25a35087fa2e9e
Felix Hermes <felixhermes@hotmail.de>
2021-06-14 14:58:25 +0200

    fast api file

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import wildfire_prediction.RF_model as rf
+from google.cloud import storage
+import joblib
+
+BUCKET_NAME = 'wildfires_le_wagon'
+BUCKET_TRAIN_DATA_PATH = 'merged_data/merged_file.csv'
+MODEL_NAME = 'wildfire prediction'
+STORAGE_LOCATION = 'models/wildfire_prediction/model_binary.joblib'
 
 app = FastAPI()
 
@@ -11,5 +19,12 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 @app.get("/")
-def index():
-    return {"greeting": "Hello world"}
+def predict_fire():
+    client = storage.Client()
+    bucket = client.get_bucket(BUCKET_NAME)
+    blob = bucket.blob(STORAGE_LOCATION)
+    blob.blob.download_to_filename('model_binary.joblib')
+    rf_model =joblib.load('model_binary.joblib')
+    
+    probability = rf.predict_proba_rf(rf_model, 'DATAFRAME_FROM_API')
+    return {"probability": probability}

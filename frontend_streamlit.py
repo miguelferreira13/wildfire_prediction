ed0972d712fa8f200b84839be692dc2ac6cd9877
Krystyna Kooi <krystynkooi@hotmail.com>
2021-06-14 10:12:41 +0200

    corrected error

--- frontend_streamlit.py
+++ frontend_streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_folium import folium_static
+#from streamlit_folium import folium_static
 import folium
 import pydeck as pdk
 
@@ -17,7 +17,7 @@ st.write(pdk.Deck(
             "longitude": 133.7751,
             "zoom": 3.2,
             "pitch": 50,
-        },
+        }))
 
 # coordinates_states = {'NSW':[-32.0948, 147.0100], 'NT': [-19.2300, 133.2128] ,'SA': [-30.0330, 135.4548],\
 #          'QL': [-22.2913, 144.2554], 'VI': [36.5115, 144.1652], 'TA': [-42.0117, 146.3536], 'WA': [-25.1941, 122.1754]}
@@ -43,3 +43,5 @@ st.write(pdk.Deck(
 #     tiles= 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3J5c2NhZ2UiLCJhIjoiY2twcWwzajE3MDh1YzJ2czhvdnI1bDBubCJ9.nAG4P8C2gctaVIhBhH3Z6w', 
 #     attr = "blabla",
 #     zoom_start=3.2)
+    
+#     folium_static(map_australia)

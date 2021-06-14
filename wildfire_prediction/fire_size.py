4840a0dcc1c103ca6134ce752026204c304181f4
Miguel Ferreira <82802408+miguelferreira13@users.noreply.github.com>
2021-06-14 14:08:50 +0200

    weather api and wfz data

--- wildfire_prediction/fire_size.py
+++ wildfire_prediction/fire_size.py
@@ -37,9 +37,25 @@ df.drop(columns=['max() Precipitation',
                  'Mean_estimated_fire_radiative_power',
                  'Year',
                  'Month',
-                 'Day'
+                 'Day',
+                 'Vegetation_index_variance'
                  ], inplace=True)
 
+# Forest
+df['Forest'] = df['Closed forest, evergreen, broad leaf']\
+        + df['Closed forest, deciduous broad leaf']\
+        + df['Closed forest, unknown']\
+        + df['Open forest, evergreen broad leaf']\
+        + df['Open forest, deciduous broad leaf']\
+        + df['Open forest, unknown definitions']
+
+df.drop(columns = ['Closed forest, evergreen, broad leaf',\
+        'Closed forest, deciduous broad leaf',\
+        'Closed forest, unknown',\
+        'Open forest, evergreen broad leaf',\
+        'Open forest, deciduous broad leaf',\
+        'Open forest, unknown definitions'],\
+        inplace= True)
 
 #Replacing na by 0
 df.Estimated_fire_area.fillna(0, inplace=True)
@@ -67,13 +83,13 @@ imputer = KNNImputer()
 values = imputer.fit_transform(X[X.dtypes[(df.dtypes == 'float64') | (X.dtypes == 'int64')].index])
 X[X.dtypes[(df.dtypes == 'float64') | (X.dtypes == 'int64')].index] = values
 
-#Scaler
-scaler = RobustScaler()
-scaled = scaler.fit_transform(X[X.dtypes[(df.dtypes == 'float64') | (X.dtypes == 'int64')].index])
-X[X.dtypes[(df.dtypes == 'float64') | (X.dtypes == 'int64')].index] = scaled
+#Scaler (NO NEED RANDOM FOREST)
+# scaler = RobustScaler()
+# scaled = scaler.fit_transform(X[X.dtypes[(df.dtypes == 'float64') | (X.dtypes == 'int64')].index])
+# X[X.dtypes[(df.dtypes == 'float64') | (X.dtypes == 'int64')].index] = scaled
 
 #Train Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.1)
 
 
 
@@ -84,10 +100,10 @@ rdf.fit(X_train, y_train)
 print(rdf.score(X_test, y_test))
 y_pred = rdf.predict(X_test)
 print(np.sqrt((y_test-y_pred)**2).mean())
-print(cross_val_score(rdf, X_test, y_test).mean())
+# print(cross_val_score(rdf, X_test, y_test).mean())
 
-X_test.to_csv('test_X', index=False)
-y_test.to_csv('test_y', index=False)
+# X_test.to_csv('test_X', index=False)
+# y_test.to_csv('test_y', index=False)
 joblib.dump(rdf, 'wildfire_size_model.joblib')
 
 

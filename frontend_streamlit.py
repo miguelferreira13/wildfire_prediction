50542418c66a8e7d907a5a0caed649358138d69a
Krystyna Kooi <krystynkooi@hotmail.com>
2021-06-15 09:32:21 +0200

    put the city data in a folder, adjusted the streamlit file

--- frontend_streamlit.py
+++ frontend_streamlit.py
@@ -12,15 +12,9 @@ import json
 
 # with open('../wildfire_prediction/Australian_cities.json') as f:
 #     data = json.load(f)
-Cities = pd.read_csv("../wildfire_prediction/Australian_cities.csv")
+Cities = pd.read_csv("../wildfire_prediction/cities_data/Australian_cities.csv")
 df = pd.DataFrame(data=Cities, columns=['city', 'lat', 'lng', 'admin_name'])
-# from streamlit_folium import folium_static
-# import geopandas as gpd
-# import folium
-import pydeck as pdk
-# from folium.plugins import TimeSliderChoropleth
-# import seaborn as sns
-# import plotly.express as px
+
 
 st.markdown("""
     # Wildfire prediction for Australia
@@ -46,19 +40,6 @@ st.write(pdk.Deck(
             "zoom": 3.2,
             "pitch": 50,
         }))
-
-# direction = st.radio('Select map', ('Fire', 'right', 'bottom', 'left'))
-
-# st.write(direction)
-
-# if direction == 'top':
-#     st.write('🔼')
-# elif direction == 'right':
-#     st.write('▶️')
-# elif direction == 'bottom':
-#     st.write('🔽')
-# else:
-#     st.write('◀️')
 
 # coordinates_states = {'NSW':[-32.0948, 147.0100], 'NT': [-19.2300, 133.2128] ,'SA': [-30.0330, 135.4548],\
 #          'QL': [-22.2913, 144.2554], 'VI': [36.5115, 144.1652], 'TA': [-42.0117, 146.3536], 'WA': [-25.1941, 122.1754]}

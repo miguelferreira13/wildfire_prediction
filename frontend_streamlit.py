b425f0d47edf4f2350f2144706023561fcd37d8a
Miguel Ferreira <82802408+miguelferreira13@users.noreply.github.com>
2021-06-16 14:11:38 +0200

    Google maps

--- frontend_streamlit.py
+++ frontend_streamlit.py
@@ -28,93 +28,109 @@ data_folder_path = os.path.join(root_path, 'wildfire_prediction')
 data_file_path = os.path.join(data_folder_path, 'Australian_cities.csv')
 
 data = pd.read_csv(data_file_path)
+st.set_page_config(page_title="My Wildfire prediction",layout='wide')
 
 st.markdown("""
     # Wildfire prediction for Australia
     ## Data Science group project, Le-Wagon Amsterdam batch 627
     ### Felix Hermes, Miguel Ferreira & Krystyna Kooi
+    
+    ---
+    
+    
     """)
 
-CITY = st.text_input('Forecast', 'Type in an Australian city name')
-HORIZON = st.text_input('Horizon', 'Type in the amount of days')
+col1, col2, col3, col4 = st.beta_columns([1,4,10,1])
 
-if CITY == 'Type in an Australian city name':
-    st.write('Waiting for Forecast')
-elif  CITY in list(data['city']):
-    st.write(f'The forecast for {CITY} is')
-    #Weather API
-else:
-    st.write('This is not a city in Australia')
+col1.markdown("")
+col1.markdown("")
+col1.markdown(":cityscape:")
+col1.markdown("")
+col1.markdown("")
+col1.markdown("")
+col1.markdown("")
+col1.markdown(":stopwatch:")
+CITY = col2.selectbox('Select a city', tuple(['Click here'] + list(data['city'])))
+# CITY = col2.text_input('Forecast', 'Type in an Australian city name')
+
+HORIZON = col2.slider('Horizon', 1, 16)
+
+# if CITY == 'Type in an Australian city name':
+#     col2.write('Waiting for Forecast')
+# elif  CITY.title() in list(data['city']):
+#     col2.write(f'The forecast for {CITY.title()} is')
+#     #Weather API
+# else:
+#     col2.write('This is not a city in Australia.')
 
 button_pressed = False
-if st.button('click me'):
+if CITY != 'Click here':
     button_pressed = True
-
-
-#layer = pdk.Layer(
-           # "ScatterplotLayer",
-            #data = df,
-        #    pickable=True,
-         #   opacity=0.8, #input here for probability
-          #  stroked=True,
-           # filled=True,
-            #radius_min_pixels=1,
-        #    radius_max_pixels=100000,
-         #   line_width_min_pixels=1,
-          #  get_position=coordinates,
-           # get_radius='normValue', #input will be the firesize
-            #get_fill_color=[255, 140, 0],
-            #get_line_color=[0, 0, 0]
-       # )
-#Key = 'pk.eyJ1Ijoia3J5c2NhZ2UiLCJhIjoiY2twcWwzajE3MDh1YzJ2czhvdnI1bDBubCJ9.nAG4P8C2gctaVIhBhH3Z6w'
-
-#st.pydeck_chart(pdk.Deck(
-    #    map_style='mapbox://styles/mapbox/satellite-streets-v11',
-     #   layers = [layer],
-      #  initial_view_state=pdk.ViewState(
-       #     mapboxApiAccessToken= Key,
-        #    latitude = -25.2744,
-         #   longitude =  133.7751,
-          #  zoom =  3.2,
-           # pitch =  50,
-     #   )
-   # ))
-
-# "# streamlit-folium"
-
-# with st.echo():
-
-#     map_australia = folium.Map(location=[-25.2744, 133.7751], 
-#     tiles= 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3J5c2NhZ2UiLCJhIjoiY2twcWwzajE3MDh1YzJ2czhvdnI1bDBubCJ9.nAG4P8C2gctaVIhBhH3Z6w', 
-#     attr = "blabla",
-#     zoom_start=3.2)
-    
-#     folium_static(map_australia)
-
-# coordinates_states = {'NSW':[-32.0948, 147.0100], 'NT': [-19.2300, 133.2128] ,'SA': [-30.0330, 135.4548],\
-#          'QL': [-22.2913, 144.2554], 'VI': [36.5115, 144.1652], 'TA': [-42.0117, 146.3536], 'WA': [-25.1941, 122.1754]}
-
 
 horizon = 1 if HORIZON == None or HORIZON == 'Type in the amount of days' else int(HORIZON)
 data_api = predict_fire(horizon)
 sizes = data_api['size']
 probabilities = data_api['probability']
 
+basemaps = {
+        'Google Maps': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Maps',
+            overlay = True,
+            control = True
+        ),
+        'Google Satellite': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Satellite',
+            overlay = True,
+            control = True
+        ),
+        'Google Terrain': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Terrain',
+            overlay = True,
+            control = True
+        ),
+        'Google Satellite Hybrid': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Satellite',
+            overlay = True,
+            control = True
+        ),
+        'Esri Satellite': folium.TileLayer(
+            tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr = 'Esri',
+            name = 'Esri Satellite',
+            overlay = True,
+            control = True
+        )
+    }
 
 
-
-with st.map():
+with col3:
     coordinates_aus = [-25.2744, 133.7751]
 
-    m = folium.Map(tiles='Stamen Terrain',location=coordinates_aus, zoom_start=3.5)
-    for i in range(7):
-        folium.Circle(coordinates[i]['coordinates'],
-                      fill=True,
-                      fill_color='crimson',
-                      color='red',
-                      radius=sizes[i]*500,
-                      popup=f'The probability of a fire is {probabilities[i][1]:.1%}\
-                          and an estimated size of {sizes[i]:.1f} km_2').add_to(m)
+    m = folium.Map(location=coordinates_aus, zoom_start=4)
+    if not button_pressed:
+        for i in range(7):
+            folium.Circle(coordinates[i]['coordinates'],
+                        fill=True,
+                        fill_color='crimson',
+                        color='crimson',
+                        radius=sizes[i]*800,
+                        popup=f"State: {coordinates[i]['state'].upper()}\n\
+                            Fire_probability: {probabilities[i][1]*0.8:.1%}\n\
+                            Estimated_size: {sizes[i]:.1f} km_2").add_to(m)
+            
+
+    # Add custom basemaps
+    basemaps['Google Maps'].add_to(m)
+    basemaps['Google Satellite Hybrid'].add_to(m)
+
     
     if button_pressed:
         city_api = predict_city(horizon, CITY)
@@ -124,10 +140,13 @@ with st.map():
         folium.Circle(list(city_coordinates),
                       fill=True,
                       fill_color='crimson',
-                      color='red',
-                      radius=sizes_city*500,
-                      popup=f'The probability of a fire is {probabilities_city:.1%}\
-                          and an estimated size of {sizes_city:.1f} km_2').add_to(m)
+                      color='crimson',
+                      radius=sizes_city*800,
+                      popup=f'City: {CITY.title()}\n\
+                          Fire_probability: {probabilities_city:.1%}\n\
+                          Estimated_size: {sizes_city*0.8:.1f} km_2').add_to(m)
+        basemaps['Google Maps'].add_to(m)
+        basemaps['Google Satellite Hybrid'].add_to(m)
     
     folium_static(m)
 
